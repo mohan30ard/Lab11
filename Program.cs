f56d552452d7b078ac624f1a07d0c13a371fcf10
Mohan <n01620006@humber.ca>
2024-04-01 23:42:48 -0400

    code

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using week11.Models.Entities;
+
+namespace Week10.Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var dbContext = new MMABooksContext())
+            {
+                var customers = dbContext.Customers.ToList();
+
+                foreach (var customer in customers)
+                {
+                    Console.WriteLine($"{customer.Name}");
+                }
+            }
+        }
+    }
+}
